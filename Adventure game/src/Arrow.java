be69d3c12fad6e54519eaf74a87807cd1757be3d
pdp5144 <122904259+pdp5144@users.noreply.github.com>
2023-01-27 17:58:38 -0500

    Add files via upload

---
+++ Adventure game/src/Arrow.java
@@ -0,0 +1,9 @@
+public class Arrow extends Weapon {
+    public Arrow(String name, String desc, int value, int damage){
+        super("Arrow","More Effective than a mace",20,40);
+    }
+
+}
+
+
+
---
+++ Adventure game/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Gold goldbar = new Gold(20);
+        Sword magicSword = new Sword();
+        Pillow softPillow = new Pillow();
+        Mace bigMace = new Mace();
+
+
+        System.out.println(goldbar);
+        System.out.println(magicSword);
+        System.out.println(softPillow);
+        System.out.println(bigMace);
+
+
+    }
+}
