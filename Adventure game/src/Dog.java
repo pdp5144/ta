143ac30f4d86d808d59ef11bf07e6639630de01a
pdp5144 <122904259+pdp5144@users.noreply.github.com>
2023-02-05 21:58:23 -0500

    Update Dog.java

--- Adventure game/src/Dog.java
+++ Adventure game/src/Dog.java
@@ -1,6 +1,9 @@
 public class Dog extends Enemy{
+    private String desc;
 
-    public Dog(){
-        super("spotDog",20,10);
+    public Dog(String desc){
+        super("spotDog","Dog can see in the dark",20,10);
+        this.desc = desc;
+
     }
 }
