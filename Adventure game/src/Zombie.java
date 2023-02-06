8cc9899dde6cea7bd58c52c02a66c09cad6797c9
pdp5144 <122904259+pdp5144@users.noreply.github.com>
2023-02-05 21:57:48 -0500

    Update Zombie.java

--- Adventure game/src/Zombie.java
+++ Adventure game/src/Zombie.java
@@ -1,6 +1,13 @@
 public class Zombie extends Enemy{
+    private int SPEED;
 
-    public Zombie() {
-        super("Zombie",15,10);
+    public Zombie(int SPEED) {
+        super("Zombie","It has Banded on his Body",15,10);
+        this.SPEED = SPEED;
     }
+
 }
+
+
+
+
