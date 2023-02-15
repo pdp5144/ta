76ebddd91a47024c390c245cbaec99e856a112cf
pdp5144 <122904259+pdp5144@users.noreply.github.com>
2023-02-15 10:29:13 -0500

    Update Main.java

--- Adventure game/src/Main.java
+++ Adventure game/src/Main.java
@@ -5,12 +5,20 @@ public class Main {
         Sword magicSword = new Sword();
         Pillow softPillow = new Pillow();
         Mace bigMace = new Mace();
+
         Ogre greenOgre = new Ogre("Green","20 inch");
         Ogre redOgre = new Ogre("Red","30 inch");
         Zombie slowZombie = new Zombie(15);
         Zombie fastZombie = new Zombie(25);
         Dog spotDog = new Dog("Dog can see in the dark");
         Kaido Emperor = new Kaido("Can turn into Demonic Dragon");
+        MoveEast moveEast = new MoveEast();
+        MoveWest moveWest = new MoveWest();
+        MoveSouth moveSouth = new MoveSouth();
+        MoveNorth moveNorth = new MoveNorth();
+        Action flee = new Flee();
+        Action Jump = new Jump();
+        Action viewInventory = new ViewInventory();
 
 
 
@@ -24,6 +32,15 @@ public class Main {
         System.out.println(fastZombie);
         System.out.println(spotDog);
         System.out.println(Emperor);
+        System.out.println(moveEast.Str());
+        System.out.println(moveWest.Str());
+        System.out.println(moveSouth.Str());
+        System.out.println(moveNorth.Str());
+        System.out.println(flee.Str());
+        System.out.println(Jump.Str());
+        System.out.println(viewInventory.Str());
+
+
 
 
 
