6b4354feaa5b132830034d1dc6d7872a3127b8bc
pdp5144 <pdp5144@psu.edu>
2023-03-25 19:43:30 -0400

    ok

---
+++ src/main/java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        Calculate calc = new Calculate();
+
+        System.out.println(calc.add(20.0, 40.0));
+        System.out.println(calc.sub(20.0, 40.0));
+        System.out.println(calc.mul(20.0, 40.0));
+        System.out.println(calc.div(20.0, 40.0));
+
+    }
+}
